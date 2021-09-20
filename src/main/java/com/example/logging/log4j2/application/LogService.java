9c783d376f5fdb3e473d21500097c7afec9731c5
Jinhong <xlffm3@gmail.com>
2021-09-21 00:23:06 +0900

    refactor: 반복문 횟수 상향 조정

--- src/main/java/com/example/logging/log4j2/application/LogService.java
+++ src/main/java/com/example/logging/log4j2/application/LogService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class LogService {
 
     public void log() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10_000; i++) {
             log.info("info log");
         }
     }
