d2a24512429d5676b90cc6c5148aa27e9e9c2454
Jinhong <xlffm3@gmail.com>
2021-09-29 01:37:23 +0900

    feat: exception handler

---
+++ src/main/java/com/example/logging/log4j2/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.logging.log4j2.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception e) {
+        log.error("{}, {}", e, e.getMessage());
+        return ResponseEntity.internalServerError()
+            .body("no");
+    }
+}
